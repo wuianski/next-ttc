860faffb0b3094d876744396e4912f57db4ccd13
wuianski <wuiyeh@gmail.com>
2023-01-12 16:04:36 +0800

    update next version for security reason

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
+  // webpack: (config, { isServer }) => {
+  // Fixes npm packages that depend on `fs` module
+  // if (!isServer) {
+  //   config.node = {
+  //     fs: 'empty'
+  //   }
+  // }
 
-    return config
-  }
+  //   return config
+  // }
 };
